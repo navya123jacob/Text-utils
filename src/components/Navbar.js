f904d17b119d5ef0a77547fc07e1253616b9f45c
navya123jacob <navyatjacob@gmail.com>
2024-03-07 22:01:50 +0530

    route correction

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,7 +19,7 @@ function Navbar(props){
         <>
         <nav className={`navbar navbar-expand-lg bg-${props.mode} navbar-${props.mode}`}>
   <div className="container-fluid">
-    <Link className="navbar-brand" to="/Text-utils">{props.title}</Link>
+    <Link className="navbar-brand" to="/">{props.title}</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -27,7 +27,7 @@ function Navbar(props){
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         
         <li className="nav-item">
-          <Link className="nav-link" to="/Text-utils/about">About</Link>
+          <Link className="nav-link" to="/about">About</Link>
         </li>
         
         
